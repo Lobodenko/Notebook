516e527e7784ba93b6ebbb6bb8f0280d656b429e
Yuliia Lobodenko <lobodenko.yulia11@gmail.com>
2024-03-08 18:01:54 +0200

    Add project files.

---
+++ Notebook/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Notebook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.noteTextBox = new System.Windows.Forms.TextBox();
+            this.notesListBox = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // noteTextBox
+            // 
+            this.noteTextBox.Location = new System.Drawing.Point(73, 41);
+            this.noteTextBox.Name = "noteTextBox";
+            this.noteTextBox.Size = new System.Drawing.Size(270, 22);
+            this.noteTextBox.TabIndex = 0;
+            // 
+            // notesListBox
+            // 
+            this.notesListBox.FormattingEnabled = true;
+            this.notesListBox.ItemHeight = 16;
+            this.notesListBox.Location = new System.Drawing.Point(444, 41);
+            this.notesListBox.Name = "notesListBox";
+            this.notesListBox.Size = new System.Drawing.Size(306, 212);
+            this.notesListBox.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(39, 300);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 79);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(299, 300);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 79);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Delete";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(550, 300);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(200, 79);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "View";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.notesListBox);
+            this.Controls.Add(this.noteTextBox);
+            this.Name = "Form1";
+            this.Text = "Notebook";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox noteTextBox;
+        private System.Windows.Forms.ListBox notesListBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Notebook/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Notebook
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+      
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (!string.IsNullOrWhiteSpace(noteTextBox.Text))
+            {
+                notesListBox.Items.Add(noteTextBox.Text);
+                noteTextBox.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Enter the note text.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (notesListBox.SelectedIndex != -1)
+            {
+                notesListBox.Items.RemoveAt(notesListBox.SelectedIndex);
+            }
+            else
+            {
+                MessageBox.Show("Select the note to delete.");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (notesListBox.SelectedIndex != -1)
+            {
+                string selectedNote = notesListBox.SelectedItem.ToString();
+                MessageBox.Show(selectedNote, "View notesи");
+            }
+            else
+            {
+                MessageBox.Show("Select a note to view.");
+            }
+        }
+    }
+}
